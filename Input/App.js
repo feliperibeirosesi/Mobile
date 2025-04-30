217839462415c37e91df61f6003846dcabccf73b
feliperibeirosesi <felipejosenew@gmail.com>
2025-04-30 15:41:56 -0300

    foi?

--- Input/App.js
+++ Input/App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-web";
 
 export default function App() {
   return (
     <View style={[styles.container, { backgroundColor: "#1d97f1" }]}>
       {/* <Text style={styles.text}>No me mires começo con ojos</Text> */}
-      <View style={[styles.card, { backgroundColor: "#f2e1ae" }]}>
-        <Text style={styles.text}>Card1</Text>
-      </View>
-      <View style={[styles.card, { backgroundColor: "#f2e1ae" }]}>
-        <Text>Card2</Text>
-      </View>
-      <View style={[styles.card, { backgroundColor: "#f2e1ae" }]}>
-        <Text>Card3</Text>
-      </View>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input} />
     </View>
   );
 }
@@ -33,7 +25,7 @@ const styles = StyleSheet.create({
     textShadowRadius: 20,
     textShadowOffset: { width: 2, height: 1 },
   },
-  card: {
+  input: {
     height: 250,
     width: 120,
   },
