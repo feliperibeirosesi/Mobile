5c9c47709fa453bea41ee921b2f3ce91da873ccb
feliperibeirosesi <felipejosenew@gmail.com>
2025-09-13 13:32:32 -0300

    Acabei o carrosel, agora vou refazer para melhorar meu aprendizado

--- AtividadeCarrosselDeFilmes/SRC/Pages/Carrosel.js
+++ AtividadeCarrosselDeFilmes/SRC/Pages/Carrosel.js
@@ -1,46 +1,78 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState, useRef } from "react";
+import { View, Text, FlatList, Image, TouchableOpacity, Dimensions, ImageBackground } from "react-native";
 
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
-
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedScrollHandler,
-  interpolate
-} from 'react-native-reanimated';
-import axios from 'axios';
-
+const { width } = Dimensions.get("window");
 
 export default function Carrosel() {
   const [movies, setMovies] = useState([]);
+  const [currentMovie, setCurrentMovie] = useState();
+
+
+  const viewConfigRef = useRef({ viewAreaCoveragePercentThreshold: 90 });
+
+  const onViewRef = useRef(({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setCurrentMovie(viewableItems[0].item);
+    }
+  });
 
   useEffect(() => {
     fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=5ca241198e38af2ff0109fa33bb8453f&language=pt-BR&page=1"
     )
       .then((res) => res.json())
-      .then((data) => setMovies(data.results))
+      .then((data) => {
+        setCurrentMovie(data.results[0]);
+        setMovies(data.results)
+      })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+
+    <ImageBackground
+      source={{ uri: `https://image.tmdb.org/t/p/w500${currentMovie?.backdrop_path}` }}
+      resizeMode="cover"
+      style={{ flex: 1, paddingVertical: 20, }}
+      blurRadius={1} // dá um efeito de "cartaz borrado" atrás
+    >
+
       <FlatList
         data={movies}
         keyExtractor={(item) => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        snapToAlignment="center"
+        decelerationRate="fast"
         renderItem={({ item }) => (
-          <TouchableOpacity style={{ marginBottom: 20 }}>
-            <Text style={{ fontSize: 18 }}>{item.title}</Text>
-            <Text>{item.release_date}</Text>
+          <TouchableOpacity
+            style={{
+              width: width * 0.8,
+              marginHorizontal: width * 0.1 / 2,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
             <Image
               source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
-              style={{ width: 120, height: 180 }}
+              style={{
+                width: "100%",
+                height: 400,
+                borderRadius: 20,
+                marginBottom: 10,
+              }}
+              resizeMode="cover"
             />
+            <Text style={{ fontSize: 18, fontWeight: "bold", color: "#fff" }}>
+              {item.title}
+            </Text>
+            <Text style={{ color: "#aaa" }}>{item.release_date}</Text>
           </TouchableOpacity>
         )}
+        onViewableItemsChanged={onViewRef.current}
+        viewabilityConfig={viewConfigRef.current}
       />
-    </View>
+    </ImageBackground>
   );
 }
